c4f46540fb7b33ed73eed452cbe099b6abacec38
Rodolfo Sousa <rodolfozsousa@gmail.com>
2023-03-31 17:42:04 -0300

    refactor: tweet controller

--- api/src/main/java/com/tweteroo/api/controllers/TweetController.java
+++ api/src/main/java/com/tweteroo/api/controllers/TweetController.java
@@ -9,16 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tweteroo.api.dto.TweetDTO;
 import com.tweteroo.api.models.Tweet;
 import com.tweteroo.api.repositories.TweetRepository;
-import com.tweteroo.api.repositories.UserRepository;
 
 @RestController
 @RequestMapping("/api/tweets")
 public class TweetController {
   @Autowired
   private TweetRepository tweetRepository;
-
-  @Autowired
-  private UserRepository userRepository;
 
   @PostMapping
   public String createTweet(@RequestBody TweetDTO req) {
