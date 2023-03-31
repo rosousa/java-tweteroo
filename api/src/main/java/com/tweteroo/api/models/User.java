aff5208c1f1027a90efbfb48c698798fdad59d4d
Rodolfo Sousa <rodolfozsousa@gmail.com>
2023-03-31 16:47:35 -0300

    feat: add user model

---
+++ api/src/main/java/com/tweteroo/api/models/User.java
@@ -0,0 +1,22 @@
+package com.tweteroo.api.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @Column(length = 50, nullable = false)
+  private String username;
+  
+  @Column(nullable = false)
+  private String avatar;
+}
