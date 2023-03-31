803ebef583ba08cfd48e9034b1ed233cb1d7bcf3
Rodolfo Sousa <rodolfozsousa@gmail.com>
2023-03-31 16:50:45 -0300

    feat: add tweet model

---
+++ api/src/main/java/com/tweteroo/api/models/tweet.java
@@ -0,0 +1,25 @@
+package com.tweteroo.api.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class tweet {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @Column(length = 50, nullable = false)
+  private String username;
+
+  @Column(nullable = false)
+  private String avatar;
+
+  @Column(length = 300, nullable = false)
+  private String text;
+}
